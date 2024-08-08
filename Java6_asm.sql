ea0ebd3b6dcc582411d8e0d207400f87c4142f53
QcVu <121864763+QcVu@users.noreply.github.com>
2024-08-08 21:17:55 +0700

    Add files via upload

---
+++ Java6_asm.sql
@@ -0,0 +1,19 @@
+﻿CREATE DATABASE ASSIGNMENT_JAVA6
+GO
+
+USE ASSIGNMENT_JAVA6
+GO
+
+CREATE TABLE JAVA6_FINAL
+(
+	ID NVARCHAR(20) PRIMARY KEY,
+	PASSWORD NVARCHAR(50),
+	FULLNAME NVARCHAR(50),
+	EMAIL NVARCHAR(50),
+	ADMIN NVARCHAR(20)
+)
+
+INSERT INTO JAVA6_FINAL VALUES 
+('admin', 'adminpass', 'A Đê Min', 'adminmail1@gmail.com', 'Admin'),
+('user', 'userpass', 'Du Se', 'usermail1@gmail.com', 'User'),
+('khachhang', 'khachhangpass', 'Khách hàng', 'khachhangmail1@gmail.com', 'Khach')
